3026666234d09d982d61de048c6c6ccbce387d44
larsonn <lars@feldeisen.de>
2021-06-18 22:40:40 +0200

    Update index.js

--- middlewares/socket/index.js
+++ middlewares/socket/index.js
@@ -1,6 +1,15 @@
 "use strict";
-
+const fs = require('fs');
+const path = require('path');
 const { sanitizeEntity } = require("strapi-utils");
+
+const loadSettings = (appPath) => {
+	const routesPath = path.join(appPath, 'extensions', 'socket', 'services', 'config.json');
+	if(fs.existsSync(routesPath)) {
+		return require(routesPath);
+	}
+	return null;
+}
 
 module.exports = (strapi) => {
   return {
@@ -8,6 +17,9 @@ module.exports = (strapi) => {
       strapi.config.middleware.load.after.unshift("socket");
     },
     initialize() {
+      const settings  = loadSettings(strapi.config.appPath);
+      let settingsRoutes = settings?.routes;
+      if(!Array.isArray(settingsRoutes)) settingsRoutes = null;
       const { actions } = strapi.plugins.socket.services.socket;
       strapi.app.use(async (ctx, next) => {
         await next();
@@ -18,16 +30,29 @@ module.exports = (strapi) => {
               route.controller in strapi.controllers &&
               actions().includes(route.action) === true
             ) {
-              strapi.StrapIO.emit(
-                strapi.controllers[route.controller],
-                route.action,
-                ctx.response.body
-              );
+              if(settingsRoutes) {
+                const isValidRoute = settingsRoutes.filter(sr => {
+                  return sr.apiName === route.controller;
+                });
+                
+                if(isValidRoute.length === 0) return;
+              }
+                strapi.StrapIO.emit(
+                  strapi.controllers[route.controller],
+                  route.action,
+                  ctx.response.body
+                );
             }
           } else if (route.controller === "collection-types") {
             let model = strapi.getModel(ctx.params.model);
             let action;
             let data;
+            if(settingsRoutes) {
+              const isValidRoute = settingsRoutes.filter(sr => {
+                return sr.apiName === model.apiName;
+              });
+              if(isValidRoute.length === 0) return;
+            }
 
             if (route.action === "bulkdelete") {
               action = "delete";
